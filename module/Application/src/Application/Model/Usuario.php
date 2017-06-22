2dc175452eef092ce63e8dc0e3a5d0c7fb3b4ae1
Diego Abanto <diego.abanto@ec.pe>
2017-06-22 17:10:15 -0500

    Modelos y entidades parte 1

--- module/Application/src/Application/Model/Usuario.php
+++ module/Application/src/Application/Model/Usuario.php
@@ -1,16 +1,21 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+namespace Application\Model;
 
-/**
- * Description of Usuario
- *
- * @author dabanto
- */
-class Usuario {
-    //put your code here
+class Usuario{
+        
+        public $id, $name, $surname, $description, $email, $password, $image, $alternative;
+
+        public function exchangeArray($data){
+                //En lugar de GETTER o SETTER se utiliza la sgte forma
+            
+                $this->id = (!empty($data['id']) ? $data['id'] : null );
+                $this->name = (!empty($data['name']) ? $data['name'] : null );
+                $this->surname = (!empty($data['surname']) ? $data['surname'] : null );
+                $this->description = (!empty($data['description']) ? $data['description'] : null );
+                $this->email = (!empty($data['email']) ? $data['email'] : null );
+                $this->password = (!empty($data['password']) ? $data['id'] : null );
+                $this->image = (!empty($data['image']) ? $data['image'] : null );
+                $this->alternative = (!empty($data['alternative']) ? $data['alternative'] : null );
+        }
 }
---
+++ module/Application/src/Application/Model/UsuariosTable.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Application\Model;
+
+use Zend\Db\TableGateway\TableGateway;
+
+class UsuariosTable {
+    
+        protected $tableGateway;
+
+        public function __construct(TableGateway $tableGateway) {
+                $this->tableGateway = $tableGateway;
+        }
+        
+        // 1. Entrar al TableGateway
+        // 2. Hacer uso de la entidad usuario
+        // 3. Hacer un select y sacar todos los registro de esa tabla
+        public function fetchAll(){
+                $resultSet =  $this->tableGateway->select();
+                return $resultSet;
+        }
+        
+        // Obtener un solo usuario
+        public function getUsuario($id){
+                $id = (int) $id;
+                
+                $rowset = $this->tableGateway->select(array("id" => $id));
+                $row = $rowset->current();
+                
+                return  $row;
+        }
+        
+        public function saveUsuario(Usuario $usuario){
+                $data = array(
+                        "name" => $usuario->name,
+                        "surname" => $usuario->surname,
+                        "description" => $usuario->description,
+                        "email" => $usuario->email,
+                        "password" => $usuario->password,
+                        "image" => $usuario->image,
+                        "alternative" => $usuario->alternative,
+                );
+                
+                $id = (int) $usuario->id;
+                
+                if($id==0){
+                        $return = $this->tableGateway->insert($data);
+                } else {
+                        if($this->getUsuario($id)){
+                                $return = $this->tableGateway->update($data);
+                        } else{
+                            throw new \Exception("El usuario no existe");
+                        }
+                }
+                
+                return $return;
+        }
+    
+        public function deleteUsuario($id){
+                $delete  = $this->tableGateway->delete(array("id" => $id));
+        
+                return $delete;
+        }
+}
