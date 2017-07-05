4c8a8ec054712efc5e6f31efec518884b7542d62
Diego Abanto <diego.abanto@ec.pe>
2017-07-05 16:25:29 -0500

    Ejemplo completo : listar usuarios

--- module/Application/src/Application/Controller/EjemploController.php
+++ module/Application/src/Application/Controller/EjemploController.php
@@ -7,10 +7,28 @@ use Zend\View\Model\ViewModel;
 
 class EjemploController extends AbstractActionController
 {
+        protected $usuariosTable;   
+        
+        protected function getUsuariosTable(){
+                if(!$this->usuariosTable){
+                        //$sm = $this->getServiceLocator();
+                        $sm =  $this->serviceLocator;
+                        $this->usuariosTable = $sm->get("Application\Model\UsuariosTable");
+                }
+                
+                return $this->usuariosTable;
+        }
+
         public function indexAction()
         {
-                echo "Ejemplo";
-                die();
+                //echo "Ejemplo";
+                
+                $usuarios = $this->getUsuariosTable()->fetchAll();
+            
+                return new ViewModel(array(
+                        "usuarios" => $usuarios
+                ));
+                
         }
     
     
